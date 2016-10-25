23abcb5141f0000a54f5f82a2ab2e40286e172d8
Sir Cinnamon <sircinnamon@gmail.com>
2016-10-25 14:05:23 -0600

    Completed bonus 1: Uses pokeapi

--- Pokemon.java
+++ Pokemon.java
@@ -1,68 +1,137 @@
 import java.util.Scanner;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.lang.StringBuilder;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.*;
 public class Pokemon
 {
+	public static final String API = "https://pokeapi.co/api/v2";
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		System.out.println("<Atk type> -> <Def Types>");
+		System.out.println("<Atk type/move> -> <Def Types/Pokemon>");
 		String input = scan.nextLine().toLowerCase();
 		while(!input.equals(""))
 		{
-			String move = input.split(" ")[0];
-			String[] defender = input.replaceAll(".*-> ", "").split(" ");
-			if(getID(move) == -1){System.out.println("Invalid."); System.exit(1);}
-			for(String d : defender)
+			String atk = input.split(" ->")[0];
+			String def = input.split(" -> ")[1];
+			//check if atk is type or move
+			//if move, replace with type
+			if(!isType(atk))
 			{
-				if(getID(d) == -1){System.out.println("Invalid."); System.exit(1);}
+				try
+				{
+					JSONObject move = getObject("/move/"+atk.replaceAll(" ","-")+"/");
+					atk = move.getJSONObject("type").getString("name");
+				}catch(Exception e)
+				{
+					System.out.println("Error in atk");
+					e.printStackTrace();
+				}
 			}
-			double multiply = 1.00;
-			for(String d : defender)
+			//check if def is type or pokemon
+			//if pokemon, replace with type(s)
+			if(!isType(def))
 			{
-				multiply = multiply * ((double)multiplier(move, d)/100);
+				try
+				{
+					JSONObject pokemon = getObject("/pokemon/"+def.replaceAll(" ","-")+"/");
+					def = "";
+					JSONArray arr = pokemon.getJSONArray("types");
+					for(int i = 0; i < arr.length();i++)
+					{
+						def = def + " " + arr.getJSONObject(i).getJSONObject("type").getString("name");
+					}
+				}catch(Exception e)
+				{
+					System.out.println("Error in def");
+					e.printStackTrace();
+				}
+			}
+			System.out.println("atk: "+atk);
+			System.out.println("def: "+def);
+			//compare atk types to def types and get multipliers
+			double multiply = -1;
+			try
+			{
+				multiply = calculateMultiplier(atk, def.trim().split(" "));
+			}catch(JSONException e)
+			{
+				e.printStackTrace();
 			}
 			System.out.println(multiply + "x damage!");
-			System.out.println("<Atk type> -> <Def Types>");
+			System.out.println("<Atk type/move> -> <Def Types/Pokemon>");
 			input = scan.nextLine();
 		}
 	}
 
-	public static int multiplier(String atk, String def)
+	public static JSONObject getObject(String subaddr)
 	{
-		//0=0xdmg
-		//1=1xdmg
-		//2=2xdmg
-		//.=.5xdmg
-		String[] multipliers = new String[18];
-		multipliers[0] =  "111111111111.011.1"; //nor
-		multipliers[1] =  "1..122111112.1.121"; //fir
-		multipliers[2] =  "12.1.111211121.111"; //wat
-		multipliers[3] =  "112..111021111.111"; //ele
-		multipliers[4] =  "1.21.11.2.1.21.1.1"; //gra
-		multipliers[5] =  "1..12.1122111121.1"; //ice
-		multipliers[6] =  "2111121.1...20122."; //fig
-		multipliers[7] =  "1111211..111..1102"; //poi
-		multipliers[8] =  "1212.112101.211121"; //gro
-		multipliers[9] =  "111.21211112.111.1"; //fly
-		multipliers[10] = "1111112211.11110.1"; //psy
-		multipliers[11] = "1.1121..1.211.12.."; //bug
-		multipliers[12] = "121112.1.2121111.1"; //roc
-		multipliers[13] = "011111111121121.11"; //gho
-		multipliers[14] = "1111111111111121.0"; //dra
-		multipliers[15] = "111111.11121121.1."; //dar
-		multipliers[16] = "1...121111112111.2"; //ste
-		multipliers[17] = "1.11112.11111122.1"; //fai
-		char mul = multipliers[getID(atk)].charAt(getID(def));
-		if(mul == '.'){return 50;}
-		else{return Integer.parseInt(""+mul)*100;}
+		StringBuilder page = new StringBuilder();
+		try
+		{
+			URL url = new URL(API+subaddr);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+			BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while((line = read.readLine()) != null)
+			{
+				page.append(line);
+			}
+			read.close();
+			return new JSONObject(page.toString());
+		}catch(Exception e)
+		{
+			System.out.println("Error in "+subaddr);
+			e.printStackTrace();
+		}
+		return new JSONObject();
 	}
 
-	public static int getID(String type)
+	public static boolean isType(String type)
 	{
 		String[] types = new String[]{"normal","fire","water","electric","grass","ice","fighting","poison","ground","flying","psychic","bug","rock","ghost","dragon","dark","steel","fairy"};
-		for(int i = 0; i<types.length;i++)
+		for(String t : types)
 		{
-			if(type.equals(types[i])){return i;}
+			if(type.equals(t)){return true;}
 		}
-		return -1;
+		return false;
+	}
+	
+	public static double calculateMultiplier(String atk, String[] def) throws JSONException
+	{
+		double m = 1;
+		JSONObject attackType = getObject("/type/"+atk+"/");
+		attackType = attackType.getJSONObject("damage_relations");
+		for(String s : def)
+		{
+			m = m*parseJsonMultiplier(attackType, s);
+		}
+		return m;
+	}
+
+	public static double parseJsonMultiplier(JSONObject relations, String defType) throws JSONException
+	{
+		JSONArray zerodmg = relations.getJSONArray("no_damage_to");
+		for(int i = 0; i<zerodmg.length();i++)
+		{
+			if(zerodmg.getJSONObject(i).getString("name").equals(defType)){return 0;}
+		}
+		JSONArray halfdmg = relations.getJSONArray("half_damage_to");
+		for(int i = 0; i<halfdmg.length();i++)
+		{
+			if(halfdmg.getJSONObject(i).getString("name").equals(defType)){return 0.5;}
+		}
+		JSONArray doubledmg = relations.getJSONArray("double_damage_to");
+		for(int i = 0; i<doubledmg.length();i++)
+		{
+			if(doubledmg.getJSONObject(i).getString("name").equals(defType)){return 2;}
+		}
+		return 1;
 	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Pokemon
 Dailyprogrammer 289 Easy
 Calculates damage multipliers for pokemon attakcs given types.
+Requires JSON for accessing pokeapi
+
+compile:
+javac -cp ./json-1.5-20090211.jar Pokemon.java
+run:
+java -cp json-1.5-20090211.jar:. Pokemon
